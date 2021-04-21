4dec938f98d92190452feda437db9af9b3555bbd
devonline <devonline.academy@gmail.com>
2021-04-21 17:56:02 +0300

    Add ClassFieldVsLocalVar

---
+++ src/section001_classes/ClassFieldVsLocalVar.java
@@ -0,0 +1,34 @@
+package section001_classes;
+
+public class ClassFieldVsLocalVar {
+    boolean aBoolean;
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    char aChar;
+    String string;
+    int[] array;
+    DynaArray dynaArray;
+
+    public static void main(String[] args) {
+        boolean aBoolean = false;
+        byte aByte;
+        short aShort;
+        int anInt;
+        long aLong;
+        float aFloat;
+        double aDouble;
+        char aChar;
+        String string;
+        int[] array;
+        DynaArray dynaArray;
+
+        ClassFieldVsLocalVar object = new ClassFieldVsLocalVar();
+        System.out.println(object.aBoolean);
+
+        System.out.println(aBoolean);
+    }
+}
