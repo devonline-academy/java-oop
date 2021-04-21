8823ffad9da69e49d9dffa919966b6f4edd49918
devonline <devonline.academy@gmail.com>
2021-04-21 14:28:23 +0300

    Add ObjectCreation

---
+++ src/section001_classes/ObjectCreation.java
@@ -0,0 +1,15 @@
+package section001_classes;
+
+public class ObjectCreation {
+    public static void main(String[] args) {
+        MyClass object = new MyClass();
+
+        MyClass notAnObject;
+
+        //-----------------------------------------------
+
+        int[] array = new int[5];
+
+        int[] notAnArray;
+    }
+}
