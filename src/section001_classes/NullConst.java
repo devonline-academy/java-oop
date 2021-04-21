96aaa31ac9cbda0471a3bce25c3dce04dfb53d2c
devonline <devonline.academy@gmail.com>
2021-04-21 16:34:11 +0300

    Add NullConst

---
+++ src/section001_classes/NullConst.java
@@ -0,0 +1,14 @@
+package section001_classes;
+
+public class NullConst {
+
+    public static void main(String[] args) {
+        MyClass o1 = new MyClass();
+        MyClass o2;
+        MyClass o3 = null;
+
+        System.out.println(o1);
+        System.out.println(o3);
+        //System.out.println(o2);
+    }
+}
