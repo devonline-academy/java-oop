3204f77afcc0988d47a3f133c9d0b555f0661ca7
devonline <devonline.academy@gmail.com>
2021-04-21 17:23:26 +0300

    Add DefaultValues

---
+++ src/section001_classes/DefaultValues.java
@@ -0,0 +1,36 @@
+package section001_classes;
+
+/**
+ * boolean aBoolean = false;
+ * byte aByte = 0;
+ * short aShort = 0;
+ * int anInt = 0;
+ * long aLong = 0;
+ * float aFloat = 0.0;
+ * double aDouble = 0.0;
+ * char aChar = 0;
+ * String string = null;
+ * int[] array = null;
+ * DynaArray dynaArray = null;
+ */
+public class DefaultValues {
+    boolean aBoolean;
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    char aChar;
+    String string;
+    int[] array;
+    DynaArray dynaArray;
+
+    public static void main(String[] args) {
+        DefaultValues defaultValues = new DefaultValues();
+        System.out.println(defaultValues.aBoolean);
+        System.out.println(defaultValues.anInt);
+        System.out.println(defaultValues.array);
+        System.out.println(defaultValues.dynaArray);
+    }
+}
