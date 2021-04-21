5424029d4e72e5eae3f9083643dae1036b3875da
devonline <devonline.academy@gmail.com>
2021-04-21 16:37:51 +0300

    Add if expressions for NullConst

--- src/section001_classes/NullConst.java
+++ src/section001_classes/NullConst.java
@@ -10,5 +10,11 @@ public class NullConst {
         System.out.println(o1);
         System.out.println(o3);
         //System.out.println(o2);
+        if (o3 == null) {
+            System.out.println("is null");
+        }
+        if (o1 != null) {
+            System.out.println("not null");
+        }
     }
 }
