18e41aaffdeb382547fd98295da06f1d146c25d2
devonline <devonline.academy@gmail.com>
2021-05-26 18:44:47 +0300

    Add ClassTemplate: fields and methods

---
+++ src/section001_classes/ClassTemplate.java
@@ -0,0 +1,24 @@
+package section001_classes;
+
+public class ClassTemplate {
+
+    // --------- Fields: ---------
+    // ${type} ${name};
+    int fieldName;
+
+    // ...
+
+
+    // --------- Methods: ---------
+    // ${return-type} ${name} (${param-list}), where param-list item is ${param-type} ${param-name}, separated by comma
+    void methodName(int param1Name, boolean param2Name, String param3Name /* ... */) {
+        // method body
+    }
+
+    // ...
+
+    // --------- Entry point: ---------
+    public static void main(String[] args) {
+
+    }
+}
