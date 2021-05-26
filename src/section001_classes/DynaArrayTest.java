f926296c107016ba20f6c7bf1ab1cd24c5aca274
devonline <devonline.academy@gmail.com>
2021-05-26 14:59:07 +0300

    Change DynaArrayTest version2

--- src/section001_classes/DynaArrayTest.java
+++ src/section001_classes/DynaArrayTest.java
@@ -1,16 +1,35 @@
 package section001_classes;
 
+import java.util.Arrays;
+
 public class DynaArrayTest {
     public static void main(String[] args) {
         DynaArray dynaArray1 = new DynaArray();
         DynaArray dynaArray2 = new DynaArray();
 
-        dynaArray1.add(1);
-        dynaArray1.add(2);
-
-        dynaArray2.add(34);
-
+        System.out.print("dynaArray1: ");
+        System.out.print(Arrays.toString(dynaArray1.result) + " ");
         System.out.println(dynaArray1.count);
+
+        System.out.print("dynaArray2: ");
+        System.out.print(Arrays.toString(dynaArray2.result) + " ");
+        System.out.println(dynaArray2.count);
+
+        dynaArray1.add(5);
+        dynaArray1.add(5);
+        dynaArray1.add(5);
+        dynaArray1.add(5);
+        dynaArray1.add(5);
+        dynaArray1.add(5);
+
+        System.out.println("------------------------------------");
+
+        System.out.print("dynaArray1: ");
+        System.out.print(Arrays.toString(dynaArray1.result) + " ");
+        System.out.println(dynaArray1.count);
+
+        System.out.print("dynaArray2: ");
+        System.out.print(Arrays.toString(dynaArray2.result) + " ");
         System.out.println(dynaArray2.count);
     }
 }
