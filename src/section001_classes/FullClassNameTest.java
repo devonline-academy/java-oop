bdc601a30a7c84259c69882138a5967db0783cee
devonline <devonline.academy@gmail.com>
2021-05-26 20:21:46 +0300

    Add full class name usage example, if class is not defined at the dsame package

--- src/section001_classes/FullClassNameTest.java
+++ src/section001_classes/FullClassNameTest.java
@@ -4,6 +4,8 @@ public class FullClassNameTest {
     public static void main(String[] args) {
         DynaArray dynaArray1 = new DynaArray();
 
-        section001_classes.DynaArray dynaArray2 = new section001_classes.DynaArray();
+        //section001_classes.DynaArray dynaArray2 = new section001_classes.DynaArray();
+
+        section001_classes.package2.Class1 class1 = new section001_classes.package2.Class1();
     }
 }
