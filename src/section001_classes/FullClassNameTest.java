b274977479b85b3ee0c5661c707394a22f6a53fc
devonline <devonline.academy@gmail.com>
2021-05-26 20:08:14 +0300

    Add FullClassNameTest with classes with same packages

---
+++ src/section001_classes/FullClassNameTest.java
@@ -0,0 +1,9 @@
+package section001_classes;
+
+public class FullClassNameTest {
+    public static void main(String[] args) {
+        DynaArray dynaArray1 = new DynaArray();
+
+        section001_classes.DynaArray dynaArray2 = new section001_classes.DynaArray();
+    }
+}
