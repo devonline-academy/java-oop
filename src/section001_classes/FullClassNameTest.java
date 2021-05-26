970790cf5a9b3fd7f7d3ba41daaa4a35e142f266
devonline <devonline.academy@gmail.com>
2021-05-26 20:30:33 +0300

    Change FullClassNameTest: using import instead of full class name

--- src/section001_classes/FullClassNameTest.java
+++ src/section001_classes/FullClassNameTest.java
@@ -1,11 +1,13 @@
 package section001_classes;
 
+import section001_classes.package2.Class1;
+
 public class FullClassNameTest {
     public static void main(String[] args) {
         DynaArray dynaArray1 = new DynaArray();
 
         //section001_classes.DynaArray dynaArray2 = new section001_classes.DynaArray();
 
-        section001_classes.package2.Class1 class1 = new section001_classes.package2.Class1();
+        Class1 class1 = new Class1();
     }
 }
