7928945c59303b6e700d1c6cc3e5005f53295a7f
devonline <devonline.academy@gmail.com>
2021-05-27 11:23:45 +0300

    Add JavaLang example

---
+++ src/section001_classes/JavaLang.java
@@ -0,0 +1,17 @@
+package section001_classes;
+
+import java.util.Arrays;
+
+//import java.lang.String;
+
+public class JavaLang {
+    public static void main(String[] args) {
+        DynaArray dynaArray = new DynaArray();
+        Arrays.toString(new int[0]);
+
+        java.util.Arrays.toString(new long[0]);
+
+        String st = "hello";
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
