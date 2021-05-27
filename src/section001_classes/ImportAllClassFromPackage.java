b5b2c63767c32d70d7b2b6e3278b537e85847279
devonline <devonline.academy@gmail.com>
2021-05-27 11:32:34 +0300

    Add ImportAllClassFromPackage

---
+++ src/section001_classes/ImportAllClassFromPackage.java
@@ -0,0 +1,14 @@
+package section001_classes;
+
+//import java.util.*;
+
+import java.util.Arrays;
+import java.util.BitSet;
+
+public class ImportAllClassFromPackage {
+    public static void main(String[] args) {
+        Arrays.toString(new int[0]);
+        BitSet bitSet;
+
+    }
+}
