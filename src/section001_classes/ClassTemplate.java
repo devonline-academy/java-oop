078a9b8813779bec0ee642a843eec68cdf8f74de
devonline <devonline.academy@gmail.com>
2021-05-28 19:16:00 +0300

    Update ClassTemplate: add constructors and init sections

--- src/section001_classes/ClassTemplate.java
+++ src/section001_classes/ClassTemplate.java
@@ -11,6 +11,21 @@ public class ClassTemplate {
 
     // ...
 
+    // --------- Constructors: ---------
+
+    // ${modifier} ${simple-class-name} (${param-list}),
+    // where param-list item is ${param-type} ${param-name}, separated by comma and
+    // ${modifier} is from set: [private, default/package, protected, public]
+    public ClassTemplate(int param1Name, boolean param2Name, String param3Name /* ... */) {
+        // constructor body
+    }
+
+    // ...
+
+    // --------- Init sections: ---------
+    {
+        // init section body
+    }
 
     // --------- Methods: ---------
     // ${modifier} ${return-type} ${name} (${param-list}),
