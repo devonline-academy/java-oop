d421c8e110d77ec664604e8a894b9eed135c1a53
devonline <devonline.academy@gmail.com>
2021-05-28 14:41:35 +0300

    Add example of constructor with param usage

--- src/structures/DynaArrayTest.java
+++ src/structures/DynaArrayTest.java
@@ -2,7 +2,7 @@ package structures;
 
 class DynaArrayTest {
     public static void main(String[] args) {
-        DynaArray dynaArray = new DynaArray();
+        DynaArray dynaArray = new DynaArray(0);
 
         dynaArray.add(0);
         dynaArray.add(1);
