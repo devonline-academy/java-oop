8b70a1657c9e98b71551457b44bf1049cbc25f59
devonline <devonline.academy@gmail.com>
2021-06-15 14:47:45 +0300

    Add difference between instanceof and getClass

--- src/academy/devonline/java/section031_polymorphism/GetClassUsage.java
+++ src/academy/devonline/java/section031_polymorphism/GetClassUsage.java
@@ -23,25 +23,25 @@ package academy.devonline.java.section031_polymorphism;
 public class GetClassUsage {
 
     public static void main(String[] args) {
-        Object o = new Child();
+        Object o1 = new Child();
+        Object o2 = null;
 
-        /*boolean result = o instanceof Child;
-        System.out.println(result);
-        System.out.println(o instanceof Child);
-        if (o instanceof Child) {
-            System.out.println("Child");
-        }*/
-        System.out.println(o.getClass());
-        if (o.getClass() == Child.class) {
-            System.out.println("Child");
-        }
+        // instanceof
+        // 1) Supports parent classes
+        System.out.println(o1 instanceof Parent);
+        // 2) Supports null
+        System.out.println(o2 instanceof Parent);
+
+        System.out.println("------------------------");
+
+        // getClass
+        // 1) Does not support parent classes
+        System.out.println(o1.getClass() == Parent.class);
+        // 2) Does not support null
+        System.out.println(o2.getClass() == Parent.class);
     }
 
-    private static class GrandParent /*extends Object*/ {
-
-    }
-
-    private static class Parent extends GrandParent {
+    private static class Parent /*extends Object*/ {
 
     }
 
