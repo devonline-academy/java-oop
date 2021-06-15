3ebeb95f2120c9e29198de769eb1c7f58e687dc5
devonline <devonline.academy@gmail.com>
2021-06-15 15:31:31 +0300

    Add CustomMarkerInterfaceUsage

---
+++ src/academy/devonline/java/section031_polymorphism/CustomMarkerInterfaceUsage.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.section031_polymorphism;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class CustomMarkerInterfaceUsage {
+
+    private interface MarkerInterface {
+
+    }
+
+    private static class Parent {
+
+        void doSomething() {
+            if (this instanceof MarkerInterface) {
+                System.out.println("OK");
+            } else {
+                System.out.println("FAILED");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Parent parent = new Child();
+        parent.doSomething();
+    }
+
+    private static class Child extends Parent implements MarkerInterface {
+
+    }
+}
