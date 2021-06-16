574bce389f052d50c3bb2b0282aecd7085cded75
devonline <devonline.academy@gmail.com>
2021-06-16 19:10:51 +0300

    Add QueueBasedOnArray

---
+++ src/academy/devonline/java/structures/QueueBasedOnArray.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.structures;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class QueueBasedOnArray implements DataStorage {
+
+    private int[] array;
+
+    private int size;
+
+    public QueueBasedOnArray(int size) {
+        array = new int[size];
+    }
+
+    public QueueBasedOnArray() {
+        this(5);
+    }
+
+    @Override
+    public void add(int value) {
+        if (size == array.length) {
+            grow(array.length == 0 ? 5 : array.length * 2);
+        }
+        array[size++] = value;
+    }
+
+    private void grow(int newSize) {
+        int[] newArray = new int[newSize];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+
+    @Override
+    public int get() {
+        if (size > 0) {
+            int result = array[0];
+            System.arraycopy(array, 1, array, 0, array.length - 1);
+            size--;
+            return result;
+        } else {
+            // TODO throw Exception
+            return 0;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
