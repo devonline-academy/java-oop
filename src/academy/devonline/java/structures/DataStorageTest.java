9b9637ee32b1f6d7ebb04ad7247916eb9d161c67
devonline <devonline.academy@gmail.com>
2021-06-16 19:15:33 +0300

    Add StackBasedOnArray

--- src/academy/devonline/java/structures/DataStorageTest.java
+++ src/academy/devonline/java/structures/DataStorageTest.java
@@ -23,7 +23,7 @@ package academy.devonline.java.structures;
 class DataStorageTest {
 
     public static void main(String[] args) {
-        DataStorage dataStorage = new QueueBasedOnArray(); // new Stack() or new Queue();
+        DataStorage dataStorage = new StackBasedOnArray(); // new Stack() or new Queue();
 
         for (int i = 0; i < 5; i++) {
             dataStorage.add(i);
