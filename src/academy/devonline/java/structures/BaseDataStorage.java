d75bed2cde239082ac3f278e3042fb012ff282e1
devonline <devonline.academy@gmail.com>
2021-06-25 15:28:59 +0300

    Add BaseDataStorage

--- src/academy/devonline/java/structures/BaseDataStorage.java
+++ src/academy/devonline/java/structures/BaseDataStorage.java
@@ -20,29 +20,12 @@ package academy.devonline.java.structures;
  * @author devonline
  * @link http://devonline.academy/java
  */
-public abstract class BasedOnArrayDataStorage extends BaseDataStorage {
+public abstract class BaseDataStorage implements DataStorage {
 
-    protected int[] array;
-
-    public BasedOnArrayDataStorage(int size) {
-        array = new int[size];
-    }
-
-    public BasedOnArrayDataStorage() {
-        this(5);
-    }
+    protected int size;
 
     @Override
-    public final void add(int value) {
-        if (size == array.length) {
-            grow(array.length == 0 ? 5 : array.length * 2);
-        }
-        array[size++] = value;
-    }
-
-    private void grow(int newSize) {
-        int[] newArray = new int[newSize];
-        System.arraycopy(array, 0, newArray, 0, array.length);
-        array = newArray;
+    public final int size() {
+        return size;
     }
 }
