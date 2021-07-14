b097845dcddd904c88938dacb71293a4fe7cf1b5
devonline <devonline.academy@gmail.com>
2021-07-14 20:22:59 +0300

    Use cmd line arguments

--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
+
 /**
  * Usage:
- * java -DDEVONLINE=test Program
+ * java Program 23 true false hello world
  *
  * @author devonline
  * @link http://devonline.academy/java
  */
 public class Program {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("DEVONLINE"));
+        System.out.println(Arrays.toString(args));
     }
 }
