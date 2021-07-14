1c04e4b8b96472e704f5b721856c42dc9221065f
devonline <devonline.academy@gmail.com>
2021-07-14 20:03:41 +0300

    Use Java properties

--- src/Program.java
+++ src/Program.java
@@ -1,30 +1,12 @@
 /**
  * Usage:
- * ===============================================================
- * For Windows:
- * <p>
- * SET DEVONLINE=test
- * java Program
- * <p>
- * SET DEVONLINE=qwerty
- * java Program
- * <p>
- * ===============================================================
- * For MacOS/Linux:
- * <p>
- * export DEVONLINE=test
- * java Program
- * <p>
- * export DEVONLINE=qwerty
- * java Program
- * <p>
- * ===============================================================
+ * java -DDEVONLINE=test Program
  *
  * @author devonline
  * @link http://devonline.academy/java
  */
 public class Program {
     public static void main(String[] args) {
-        System.out.println(System.getenv("DEVONLINE"));
+        System.out.println(System.getProperty("DEVONLINE"));
     }
 }
