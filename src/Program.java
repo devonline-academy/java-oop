5f4dd896641734b009cb5274b7f48b09055f569d
devonline <devonline.academy@gmail.com>
2021-07-19 14:32:16 +0300

    Get OS specific info

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,36 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+/*
+ * Copyright 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
 
 /**
  * @author devonline
  * @link http://devonline.academy/java
  */
 public class Program {
-    public static void main(String[] args) throws IOException {
-        System.out.println(Files.readString(Paths.get(System.getProperty("user.home") + "config.txt")));
+    public static void main(String[] args) {
+        // Home directory: `$HOME` or `%HOMEPATH%`
+        System.out.println(System.getProperty("user.home"));
+        // New line separator: `\n` or `\r\n`
+        System.out.println(System.lineSeparator());
+        // Path element separator: `/` or `\`
+        System.out.println(File.pathSeparator);
+        // Absolute or relative paths separator: `:` or `;`
+        System.out.println(File.separator);
+        // Read environment variable with name `USERNAME`
+        System.out.println(System.getenv("USERNAME"));
     }
 }
