304bd215cf4634781545b8435ac0adc2c2a014c9
devonline <devonline.academy@gmail.com>
2021-09-29 17:44:53 +0300

    Add StaticMethodsNotPolymorph

---
+++ src/academy/devonline/java/section051_static/StaticMethodsNotPolymorph.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.section051_static;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class StaticMethodsNotPolymorph {
+
+    static class Parent {
+
+        static void doSomething() {
+            System.out.println("Parent");
+        }
+    }
+
+    static class Child extends Parent {
+
+        static void doSomething() {
+            System.out.println("Child");
+        }
+    }
+
+    public static void main(final String[] args) {
+        final Parent[] parents = {new Parent(), new Child()};
+        for (final Parent parent : parents) {
+            parent.doSomething();
+        }
+    }
+}
