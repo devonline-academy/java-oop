a0efc77cbd2b08b46830c2c9fd116ce8f59eb551
devonline <devonline.academy@gmail.com>
2021-09-29 20:02:46 +0300

    Add Solution3 for enum problem

---
+++ src/academy/devonline/java/section061_enum/Solution3.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.section061_enum;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class Solution3 {
+
+    public enum Status {
+
+        STARTED,
+
+        IN_PROGRESS,
+
+        COMPLETED
+    }
+
+    public static class Task {
+
+        private Status status = Status.STARTED;
+
+        public void setStatus(final Status status) {
+            this.status = status;
+        }
+    }
+
+    public static void main(final String[] args) {
+        Task task = new Task();
+        //
+        task.setStatus(Status.IN_PROGRESS);
+        //
+        task.setStatus(Status.COMPLETED);
+
+        //task.setStatus(-45);
+    }
+}
